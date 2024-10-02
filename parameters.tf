6eec6472cad7ff67bc6688cac13bbaa90108b09c
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-10-02 10:02:14 -0700

    Update parameters.tf

--- parameters.tf
+++ parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "parameter" {
-  for_each = var.parameters
+  for_each = tomap({ for param in var.parameters : (param.name) => param })
 
   name  = each.value.name
   value = each.value.value

aaf56f3098323692f8d8d6742987e1bcd1bb0084
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-10-02 11:00:09 -0700

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -1,17 +1,20 @@
 resource "aws_secretsmanager_secret" "secret" {
+  count = var.init_secrets ? 1 : 0
   for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
 
   name = each.key
 }
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
+  count = var.init_secrets ? 1 : 0
   for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
 
   secret_id     = aws_secretsmanager_secret.secret[each.key].id
   secret_string = each.value.secret_string
 }
 
-data "aws_secretsmanager_secret" "lookup" {
+data "aws_secretsmanager_secret" "secret" {
+  count = var.init_secrets ? 1 : 0
   for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
 
   depends_on = [
@@ -22,7 +25,8 @@ data "aws_secretsmanager_secret" "lookup" {
   name = each.key
 }
 
-data "aws_secretsmanager_secret_version" "lookup_version" {
+data "aws_secretsmanager_secret_version" "secret_version" {
+  count = var.init_secrets ? 1 : 0
   for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
 
   depends_on = [
@@ -30,5 +34,18 @@ data "aws_secretsmanager_secret_version" "lookup_version" {
     aws_secretsmanager_secret_version.secret_version
   ]
 
+  secret_id = data.aws_secretsmanager_secret.lookup[each.key].id
+}
+
+
+data "aws_secretsmanager_secret" "secret_non_init" {
+  count = var.init_secrets ? 0 : 1
+  for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
+  name = each.key
+}
+
+data "aws_secretsmanager_secret_version" "secret_non_init_version" {
+  count = var.init_secrets ? 0 : 1
+  for_each = { for s in var.secrets : "${s.path_prefix}/${s.path_key}" => s }
   secret_id = data.aws_secretsmanager_secret.lookup[each.key].id
 }
